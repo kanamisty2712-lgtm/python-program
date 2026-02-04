e2aeec519d46567f5b6cfe5b1e5005e8a909d170
tanishk-lodh <kanamisty2712@gmail.com>
2026-02-04 08:57:37 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,42 @@
+"""age =int(input(" Enter your age :"))
+if age >=18:
+    print(" yes you are eligible...")
+else :
+    print("you are not eligible :")"""""
+    
+   #marks
+   
+"""marks =int(input("Enter your marks :"))
+if marks >=90:
+        
+       print("you will get A grade")
+elif marks >=70:
+        
+       print("you will get a B grade")
+else : 
+       print("you will get C grade ")""" 
+          
+ #Odd and Even
+"""number= int(input("Enter your number"))
+if number%2==0:
+     print("even number")
+else :
+     print("Odd number")"""
+     
+  # Negative and positive
+"""num=int(input("Enter your number"))
+if num>0:
+    print("Positive number")
+elif num<0:
+    print("Negative number")"""
+ 
+ # licsense eligible#
+"""lic=int(input("Enter your age"))
+if lic>=18:
+     print("You are eligible for licsense")
+elif lic<=18:
+     print("You are not elgible for licsense")
+else :
+     print("You are small kiddo")"""
+                      
+                
